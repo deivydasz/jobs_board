8abce68a985b4ee66848f9d8c9e9d81b0fd98e6e
ecuSinner <deivydas.ziziliauskas@gmail.com>
2016-08-19 16:14:59 +0300

    Set stage and  server at deploy/production.rb, moved database.yml and secrets.yml to examples

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,7 +1,12 @@
 # server-based syntax
+
+set :stage, :production
+server '194.135.94.6', user: 'deivis', roles: %w{web app db}
 # ======================
 # Defines a single server with a list of roles and multiple properties.
 # You can define all roles on a single server, or split them:
+
+
 
 # server 'example.com', user: 'deploy', roles: %w{app db web}, my_property: :my_value
 # server 'example.com', user: 'deploy', roles: %w{app web}, other_property: :other_value
